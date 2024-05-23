30ec880fcc13a34197223fd017acb89bc7daf449
Karthik Deivasigamani <karthikd2991@outlook.com>
2024-05-23 20:59:56 +0530

    Late

---
+++ Python/Practice_Project_1/Loop_Control_Statements.py
@@ -0,0 +1,11 @@
+# Loop Control Statemetns = Changes a loop execution from its normal sequence
+
+# break = used to terminate the loop entirely
+# continue = skips to the nnext iteration of the loop
+# pass = does nothing acts a placeholder
+
+while True:
+    name = input("Enter you name:")
+    if name != "":
+        break
+
